a507925b60e80a41f81b2fd0140b32b251b44617
asloutino <etienne.wansa@gmail.com>
2024-07-22 11:24:17 +0300

    bizmax home

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
+
+Route::get('{any}', [App\Http\Controllers\HomeController::class, 'pageView']);
