a99a55874d36d695fd006edba1958f40b11074ee
asloutino <etienne.wansa@gmail.com>
2024-07-25 19:44:14 +0300

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -529,7 +529,7 @@
                 <div class="cs_social_btns d-flex flex-wrap cs_column_gap_15 cs_row_gap_15 cs_transition_5 cs_fs_20 cs_fs_lg_18 position-absolute">
                 <!-- <a href="#" data-bs-toggle="modal" data-bs-target="#HRH-Prince-Faisal-bin-Bandar" class="d-flex align-items-center justify-content-center cs_height_45 cs_width_100 border-0 text-white cs_rounded_5">Read Bio</a> -->
                 </div>
-              </div>
+              </div> 
               <div class="cs_team_info cs_pt_127 cs_pl_15 cs_pr_15 cs_pb_25 cs_transition_4 cs_rounded_50">
                 <h2 class="text-white cs_fs_20 cs_mb_10">HRH Prince Faisal bin Bandar</h2>
                 <p class="text-white cs_fs_12 m-0 lh-base"></p>
