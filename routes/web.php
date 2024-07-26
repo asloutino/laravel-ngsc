f972b72f9c0acbff19359579ab37c613c322a530
asloutino <etienne.wansa@gmail.com>
2024-07-27 01:08:39 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,4 @@ Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
 
 Route::get('{any}', [App\Http\Controllers\HomeController::class, 'pageView']);
 
-Route::post('/contact', [App\Http\Controllers\ContactFormController::class, 'submit'])->name('contact.submit');
+Route::post('/', [App\Http\Controllers\ContactFormController::class, 'submit'])->name('contact.submit');
