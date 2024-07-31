2da227a427af58cd91fc67048cd2bbb420dc88eb
asloutino <etienne.wansa@gmail.com>
2024-07-31 21:53:38 +0300

    Press release

---
+++ resources/views/global-leaders-to-address-the-future-of-fandom-and-fan-engagement-at-ngsc.blade.php
@@ -0,0 +1,71 @@
+@extends('layout.layout')
+
+@section('content')
+
+    <x-header pageTitle="Blog Details" breadcrumbItem="Blog Details" />
+
+    <!-- Start Blog Section -->
+    <section class="cs_pt_140 cs_pt_lg_80 cs_pb_10 cs_pb_lg_10 news-article">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="cs_post cs_style_1 shadow-sm cs_mb_30">
+              <img src="{{ asset('assets/img/news/EWC_KV_MASTER.jpg') }}" alt="">
+              <div class="cs_pl_40 cs_pr_40 cs_pt_40 cs_pb_40 cs_pl_lg_25 cs_pr_lg_25 cs_pt_lg_25 cs_pb_lg_25">
+                <!-- <ul class="cs_post_meta d-flex flex-wrap cs_fs_12 p-0 cs_mb_20">
+                  <li>
+                    <span><i class="fa-solid fa-user"></i> By </span> 
+                    <a href="">admin</a>
+                  </li>
+                  <li>
+                    <span><i class="fa-solid fa-comment-dots"></i></span> 
+                    <a href="">3 comments</a>
+                  </li>
+                </ul> -->
+                <h2 class="cs_post_title cs_lh_base cs_fs_26 cs_fs_lg_18 cs_mb_20"><a href="{{ url('global-leaders-to-address-the-future-of-fandom-and-fan-engagement-at-ngsc') }}">Global leaders to address ‘The Future of Fandom’ and explore the evolving dynamics of fan engagement at New Global Sport Conference</a></h2>
+                <p>• The second edition of NGSC – focusing on esports, gaming, tech, sports and entertainment – will be held at the Four Seasons Hotel Riyadh At Kingdom Center on August 24-25</p>
+                <p>• Speakers include HRH Prince Faisal bin Bandar bin Sultan, Chairman of the Saudi Esports Federation; Ralf Reichert, CEO of the Esports World Cup Foundation; Magnus Carlsen, Norwegian chess grandmaster; and Toshimoto Mitomo, Executive Deputy President and CSO at Sony</p>
+                <p><strong>July 30, 2024: Riyadh, Saudi Arabia</strong> – The second edition of the New Global Sport Conference (NGSC) gathers visionary leaders from esports, gaming, business, mainstream sports, and entertainment to explore this year's theme ‘The Future of Fandom’.</p>
+                <p>Hosted at the Four Seasons Hotel Riyadh At Kingdom Center on August 24-25, NGSC will discuss how fandom is evolving and its significant impact on business planning, content creation, IP and media rights, marketing, and strategic community-building.</p>
+                <p>NGSC will host sessions that will highlight the challenges and opportunities in crafting a new global sporting legacy, the role of technology in shaping the future of competitive gaming, and the art of storytelling in esports.</p>
+                <p>More than 60 global leaders from their respective sectors will share insights on innovation, market expansion, and the creation of game titles designed to endure for decades. The theme of the event is especially apt considering an astounding 67% of Saudi Arabia’s native population identify as gamers.</p>
+                <p><strong>The first speakers to be announced for this year’s #NGSC24 include:</strong>
+                  <ul>
+                    <li><strong>HRH Prince Faisal bin Bandar bin Sultan</strong>, Chairman of the <strong>Saudi Esports Federation</strong></li>
+                    <li><strong>Ralf Reichert</strong>, CEO of the <strong>Esports World Cup Foundation</strong></li>
+                    <li><strong>Magnus Carlsen</strong>, World Chess Champion</li>
+                    <li><strong>Toshimoto Mitomo</strong>, Executive Deputy President and CSO, <strong>Sony</strong></li>
+                    <li><strong>Lisa Hanson</strong>, CEO, <strong>Niko Partners</strong></li>
+                    <li><strong>Andrew Chen</strong>, General Partner, <strong>Andreessen Horowitz</strong></li>
+                    <li><strong>Dr. Songyee Yoon</strong>, Managing Partner of <strong>Principal Venture Partners and Trustee of the <strong>Asian Art Museum Foundation of San Francisco</strong></li>
+                    <li><strong>Sir Leonard Blavatnik</strong>, Founder of <strong>Access Industries</strong> and Owner of <strong>DAZN</strong></li>
+                    <li><strong>Jann Mardenborough</strong>, Sim Racing & Motorsports Driver</li>
+                  </ul>
+                </p>
+                <p>Ralf Reichert, CEO of the Esports World Cup Foundation, said: “I'm thrilled about the upcoming New Global Sport Conference, which will bring together some of the smartest and most experienced leaders across esports, gaming, tech, media, sports, and entertainment, to dive into how esports redefines fan engagement. Building on the success of our first Esports World Cup and hosting it in Riyadh, we aim to spark conversations that will push the industry forward for years to come.”</p>
+                <p>The conference will focus on key themes such as ‘Fan Engagement in Sports and Esports’, exploring the latest trends and strategies. These include the use of social media, gamification, real-time interaction, personalized fan experiences, innovative marketing tactics, and AI-driven content to enhance fan loyalty and drive growth.</p>
+                <p>Speakers at the conference will also shed light on the recent news about a historic milestone for the development of esports, coming out from the International Olympic Committee (IOC), partnering with the National Olympic Committee (NOC) of Saudi Arabia to host the inaugural Olympic Esports Games 2025 in the Kingdom for the next 12 years.</p>
+                <p>NGSC’s inaugural edition last year saw the unveiling of the first Esports World Cup – currently taking place in Riyadh – and the Esports World Cup Foundation. The latter, a non-profit organization, is dedicated to fostering collaboration within the esports community and ensuring the sector's long-term sustainability. Building on the success of its inaugural event, the NGSC continues to grow, having integrated the 2022 and 2023 editions of the Next World Forum under its umbrella.</p>
+                <p>Together with the Esports World Cup and the Esports World Cup Foundation,  NGSC aligns with Saudi Arabia’s comprehensive National Gaming & Esports Strategy, which was unveiled in 2022. The strategy is aligned with Vision 2030 and highlights the Kingdom’s commitment to leadership and innovation in this global dynamic sector.</p>
+                <p>Moreover, the strategy reflects Saudi Arabia’s vision to position the country at the forefront of the global gaming landscape, create 39,000 jobs locally, and contribute $13.3 billion to the Kingdom’s economy.</p>
+                <p>For more information on the event and to stay updated on further announcements, please visit New Global Sport Conference Website.</p>
+                <p><strong>For more information regarding New Global Sport Conference, contact:</strong><br>
+                    Andrew Johnson at Burson: andrew.johnson@bursonglobal.com
+                </p>
+                <p><strong>About New Global Sport Conference</strong><br>
+                    The New Global Sport Conference (NGSC24), the main conference of the Esports World Cup Foundation (EWCF), is a gateway for today and tomorrow’s executives, global leaders, and prime decision-makers into a new global sport and will showcase how esports can create new global opportunities. The NGSC24 will unite top players from sports, esports, gaming, entertainment, technology and beyond, serving as a a content-packed platform bringing together these industries. It carries on the torch of the Next World Forum series and NGSC23, uniting both events under one exclusive banner in 2024.<br>
+                    Website:  www.newglobalsportconference.com 
+                </p>
+                <p><strong>About the Esports World Cup</strong><br>
+                    The Esports World Cup is a global celebration of competitive excellence and esports fandom. The competition features a unique cross-game structure that will pit the world’s top esports teams and athletes against one another for the largest-ever prize pool. Debuting in the summer of 2024, gamers, publishers, and fans from around the globe will come together in Riyadh, Saudi Arabia, to crown the world’s first Esports World Cup Club Champion. https://www.esportsworldcup.com/en 
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- End Blog Section -->
+    <!-- End Our Latest Project -->
+    
+    @endsection
