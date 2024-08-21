81fe5ce66dfb2526453e36b082a4187356ef6736
asloutino <etienne.wansa@gmail.com>
2024-08-21 19:18:34 +0300

    agenda

---
+++ resources/views/agenda.blade.php
@@ -0,0 +1,27 @@
+@extends('layout.layout')
+
+@section('content')
+
+    <x-header pageTitle="Privacy Policy" breadcrumbItem="Privacy Policy" />
+
+    <!-- Start Blog Section -->
+    <section class="cs_pt_140 cs_pt_lg_80 cs_pb_10 cs_pb_lg_10">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="cs_post cs_style_1 shadow-sm cs_mb_30">
+              <div class="cs_pl_40 cs_pr_40 cs_pt_40 cs_pb_40 cs_pl_lg_25 cs_pr_lg_25 cs_pt_lg_25 cs_pb_lg_25">
+                <h2 class="cs_post_title cs_lh_base cs_fs_26 cs_fs_lg_18 cs_mb_20">Preliminary Agenda</h2>
+                <img class="cs-hidden_mobile" src="assets/img/agenda/agenda.svg">
+                <img class="cs-hidden_desktop" src="assets/img/agenda/agenda-m-1.svg">
+                <img class="cs-hidden_desktop cs_mt_40" src="assets/img/agenda/agenda-m-2.svg">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- End Blog Section -->
+    <!-- End Our Latest Project -->
+    
+    @endsection
